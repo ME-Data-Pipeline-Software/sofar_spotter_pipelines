cda84f0e00f199ebb040bc9c18452bcb4edb1af2
jmcvey3 <53623232+jmcvey3@users.noreply.github.com>
2022-09-14 20:03:46 -0700

    spotter data update

--- pipelines/waves/test/test_pipeline.py
+++ pipelines/waves/test/test_pipeline.py
@@ -9,9 +9,7 @@ def test_waves_pipeline():
     pipeline = config.instantiate_pipeline()
 
     test_file = "pipelines/waves/test/data/input/0009_FLT.CSV"
-    expected_file = (
-        "pipelines/waves/test/data/expected/buoy.waves.b1.20210819.210649.nc"
-    )
+    expected_file = "pipelines/waves/test/data/expected/buoy.spotter-wave-200ms.b1.20210819.210649.nc"
 
     dataset = pipeline.run([test_file])
     expected: xr.Dataset = xr.open_dataset(expected_file)  # type: ignore
