3f09813838c544c270b49a99f79a5a495fabafc7
jmcvey3 <53623232+jmcvey3@users.noreply.github.com>
2022-09-14 19:50:08 -0700

    Update test files

--- pipelines/adv/test/test_pipeline.py
+++ pipelines/adv/test/test_pipeline.py
@@ -9,7 +9,7 @@ def test_adv_pipeline():
     pipeline = config.instantiate_pipeline()
 
     test_file = "pipelines/adv/test/data/input/vector_example.VEC"
-    expected_file = "pipelines/adv/test/data/expected/cook_inlet.adv-velocity-125ms.a1.20210701.134003.nc"
+    expected_file = "pipelines/adv/test/data/expected/cook_inlet.adv-velocity-125ms.a1.20210701.134002.nc"
 
     dataset = pipeline.run([test_file])
     expected: xr.Dataset = xr.open_dataset(expected_file)  # type: ignore
