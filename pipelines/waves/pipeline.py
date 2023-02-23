84f11ad22ff45681f4e8b0534ebe807a8c904558
jmcvey3 <53623232+jmcvey3@users.noreply.github.com>
2023-02-23 13:43:25 -0800

    Spotter buoy raw input

--- pipelines/waves/pipeline.py
+++ pipelines/waves/pipeline.py
@@ -1,9 +1,10 @@
+#import numpy as np
 import xarray as xr
-
 import matplotlib.pyplot as plt
-from tsdat import IngestPipeline, get_start_date_and_time_str, get_filename
 
-from utils import format_time_xticks
+from tsdat import IngestPipeline, get_start_date_and_time_str, get_filename
+#import dolfyn
+#from mhkit import wave
 
 
 class Waves(IngestPipeline):
@@ -21,6 +22,62 @@ class Waves(IngestPipeline):
     def hook_finalize_dataset(self, dataset: xr.Dataset) -> xr.Dataset:
         # (Optional) Use this hook to modify the dataset after qc is applied
         # but before it gets saved to the storage area
+
+        ## Analysis for VAP
+        # ds = dataset
+        # disp = xr.DataArray(
+        #     data=np.array(
+        #         [
+        #             ds["surge"],
+        #             ds["sway"],
+        #             ds["heave"],
+        #         ]
+        #     ),
+        #     coords={"dir": ["x", "y", "z"], "time": ds.time},
+        # )
+        # ## Using dolfyn to create spectra
+        # fft_tool = dolfyn.adv.api.ADVBinner(
+        #     ds.nbin, ds.fs, n_fft=ds.nbin / 3, n_fft_coh=ds.nbin / 3
+        # )
+        # psd = fft_tool.calc_psd(ds.disp, freq_units="Hz")
+        # csd = fft_tool.calc_csd(ds.disp, freq_units="Hz")
+
+        # psd = psd.sel(freq=slice(0.0455, None))
+        # Sxx = psd.sel(S="Sxx")
+        # Syy = psd.sel(S="Syy")
+        # Szz = psd.sel(S="Szz")
+        # pd_Szz = Szz.T.to_pandas()
+
+        # ## Wave analysis using MHKiT
+        # Hs = wave.resource.significant_wave_height(pd_Szz)
+        # Te = wave.resource.energy_period(pd_Szz)
+
+        # ## Wave direction and spread
+        # Cxz = csd.sel(C="Cxz").real
+        # Cyz = csd.sel(C="Cyz").real
+
+        # # Check that k is less than 1
+        # k = np.sqrt(
+        #     (Sxx + Syy) / Szz
+        # )  # wavenumber approx - need to take into account shallow water
+        # plt.figure()
+        # plt.loglog(psd['freq'], k.mean("time"))
+        # plt.xlabel("Frequency [Hz]")
+        # plt.ylabel("k [nondim]")
+
+        # a = Cxz / (k * Szz)
+        # b = Cyz / (k * Szz)
+        # theta = np.arctan(b / a) * (180 / np.pi)  # degrees CCW from East
+        # # theta = dolfyn.tools.misc.convert_degrees(theta) # degrees CW from North
+        # phi = np.sqrt(2 * (1 - np.sqrt(a**2 + b**2))) * (180 / np.pi)
+        # phi = phi.fillna(0)  # fill missing data
+
+        # direction = np.arange(len(ds['time']))
+        # spread = np.arange(len(ds['time']))
+        # for i in range(len(ds['time'])):
+        #     direction[i] = np.trapz(theta[i], psd['freq'])
+        #     spread[i] = np.trapz(phi[i], psd['freq'])
+
         return dataset
 
     def hook_plot_dataset(self, dataset: xr.Dataset):
@@ -36,9 +93,9 @@ class Waves(IngestPipeline):
         with self.storage.uploadable_dir(datastream) as tmp_dir:
             fig, ax = plt.subplots()
 
-            ax.plot(ds.time, ds.displacement.sel(dir="x"), label="x-direction")
-            ax.plot(ds.time, ds.displacement.sel(dir="y"), label="y-direction")
-            ax.plot(ds.time, ds.displacement.sel(dir="z"), label="z-direction")
+            ax.plot(ds.time, ds["surge"], label="surge")
+            ax.plot(ds.time, ds["sway"], label="sway")
+            ax.plot(ds.time, ds["heave"], label="heave")
 
             ax.set_title("")  # Remove bogus title created by xarray
             ax.legend(ncol=2, bbox_to_anchor=(1, -0.05))
--- pipelines/waves/readers.py
+++ pipelines/waves/readers.py
@@ -3,55 +3,7 @@ from pydantic import BaseModel, Extra
 import xarray as xr
 import pandas as pd
 import numpy as np
-import warnings
 from tsdat import DataReader
-
-
-class MotionReader(DataReader):
-    """---------------------------------------------------------------------------------
-    Custom DataReader that can be used to read data from a specific format.
-
-    Built-in implementations of data readers can be found in the
-    [tsdat.io.readers](https://tsdat.readthedocs.io/en/latest/autoapi/tsdat/io/readers)
-    module.
-
-    ---------------------------------------------------------------------------------"""
-
-    class Parameters(BaseModel, extra=Extra.forbid):
-        """If your CustomDataReader should take any additional arguments from the
-        retriever configuration file, then those should be specified here.
-
-        e.g.,:
-        custom_parameter: float = 5.0
-
-        """
-
-    parameters: Parameters = Parameters()
-    """Extra parameters that can be set via the retrieval configuration file. If you opt
-    to not use any configuration parameters then please remove the code above."""
-
-    def read(self, input_key: str) -> Union[xr.Dataset, Dict[str, xr.Dataset]]:
-        # Reads "FLT" filetype from spotter: wave displacement data
-        # Units are converted to m through config file
-
-        # Ignore pandas ParserWarning:
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            df = pd.read_csv(input_key, delimiter=",", index_col=False)
-        ds = xr.Dataset(
-            data_vars={
-                "displacement": (
-                    ["dir", "time"],
-                    np.array([df["outx(mm)"], df["outy(mm)"], df["outz(mm)"],]),
-                ),
-                "t_elapsed": (["time"], df["millis"]),
-            },
-            coords={
-                "dir": ("dir", ["x", "y", "z"]),
-                "time": ("time", df["GPS_Epoch_Time(s)"]),
-            },
-        )
-        return ds
 
 
 class GPSReader(DataReader):
